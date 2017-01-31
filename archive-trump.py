ce5a509d5e51beab779794a84ea7cb0709d926bf
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2017-01-31 03:56:35 -0800

    31 Jan 2017: initial commit

---
+++ archive-trump.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Uses Twitter's streaming API to back up the POTUS's tweets, hopefully before
+they're deleted.
+"""
+
+if __name__ == "__main__":
+    pass
