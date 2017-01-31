0af606763a2671916fc7e2015594a2f0078ce628
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2017-01-31 14:55:35 -0800

    31 Jan 2017: listens on @POTUS and @realDonaldTrump accounts, but doesn't yet archive

--- archive-trump.py
+++ archive-trump.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Adapted from the answer at http://stackoverflow.com/a/38192468/5562328 -- thanks 
+"""This script uses the Twitter streaming API to wait for tweets by Donald Trump,
+then causes them to be archived at the Internet Archive, hopefully before The
+Donald deletes them.
+
+First steps here were based partly on http://stackoverflow.com/a/38192468/5562328
+-- thanks kmario23.
 """
 
 
@@ -21,6 +26,7 @@ access_token = Trump_client['access_token']
 access_token_secret = Trump_client['access_token_secret']
 
 Trump_twitter_accounts = ['25073877', '822215679726100480']     # @realDonaldTrump = 25073877; @POTUS = 822215679726100480
+# Trump_twitter_accounts = ['814046047546679296']               # @false_trump = 814046047546679296
 
 class StdOutListener(StreamListener):
     '''A basic listener that just writes received tweets to file.'''
@@ -30,7 +36,8 @@ class StdOutListener(StreamListener):
 
     def on_data(self, data):
         data = json.loads(data)
-        print(data['text'])
+        if data['user']['id_str'] in Trump_twitter_accounts:
+            print(data['text'])
         return True
 
     def on_error(self, status):
@@ -48,7 +55,7 @@ if __name__ == '__main__':
             stream.filter(follow=Trump_twitter_accounts, stall_warnings=True)
 
         except IncompleteRead as e:
-            # Oh well, sleep sometime & reconnect and keep trying again
+            # Oh well. Sleep some before trying again
             time.sleep(15)
             continue
 
