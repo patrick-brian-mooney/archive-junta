94927272a5df015f61e93c4a12c12bad9c3ee427
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2017-02-08 00:06:45 -0800

    7 Feb 2017: a little closer debugging attention in some functions

--- archive_trump.py
+++ archive_trump.py
@@ -22,7 +22,7 @@ this program.
 """
 
 
-import time, json, requests, sys
+import time, json, requests, sys, pprint
 
 from tweepy.streaming import StreamListener                     # http://www.tweepy.org
 from tweepy import OAuthHandler
@@ -32,6 +32,8 @@ from http.client import IncompleteRead
 import pid                                                      # https://pypi.python.org/pypi/pid/
 
 
+import patrick_logger                                           # https://github.com/patrick-brian-mooney/python-personal-library/
+from patrick_logger import log_it
 import social_media as sm                                       # https://github.com/patrick-brian-mooney/python-personal-library/
 from social_media_auth import Trump_client                      # Unshared module that contains my authentication constants
 
@@ -48,7 +50,7 @@ archiving_url_prefixes = ['http://web.archive.org/save/']
 home_dir = '/archive-trump'
 last_tweet_id_store = '%s/last_tweet' % home_dir
 
-debugging = True
+patrick_logger.verbosity_level = 1
 
 
 def get_tweet_urls(username, id):
@@ -62,11 +64,11 @@ def archive_tweet(screen_name, id, text):
     """Have the Internet Archive (and in the future, perhaps, other archives) save
     a copy of this tweet. 
     """
-    if debugging: print("New tweet from %s: %s" % (screen_name, text))
+    log_it("New tweet from %s: %s" % (screen_name, text), 0)
     for which_url in get_tweet_urls(screen_name, id):
-        if debugging: print("\narchiving URL %s" % which_url)
+        log_it("\narchiving URL %s" % which_url, 0)
         for which_prefix in archiving_url_prefixes:
-            if debugging: print("    ... archiving using prefix %s" % which_prefix)
+            log_it("    ... archiving using prefix %s" % which_prefix)
             req = requests.get(which_prefix + which_url)
             for the_item in req.iter_content(chunk_size=100000): pass   # read the file to make the IArchive archive it.
     try:
@@ -91,9 +93,16 @@ class TrumpListener(StreamListener):
     bot to listen for us.
     """
     def on_data(self, data):
-        data = json.loads(data)
-        if data['user']['id_str'] in Trump_twitter_accounts:        # If it's one of the accounts we're watching, archive it.
-            archive_tweet(data['user']['screen_name'], data['id_str'], data['text'])
+        try:
+            data = json.loads(data)
+            if data['user']['id_str'] in Trump_twitter_accounts:        # If it's one of the accounts we're watching, archive it.
+                archive_tweet(data['user']['screen_name'], data['id_str'], data['text'])
+        except:
+            print('ERROR: \n  Exception is:')
+            pprint.pprint(sys.exc_info()[0])
+            print('  Value of data is:')
+            pprint.pprint(data)
+            raise
         return True
     def on_error(self, status):
         print("ERROR: %s" % status)
@@ -113,11 +122,11 @@ def get_new_tweets(screen_name, oldest=-1):
 
     # keep grabbing tweets until there are no tweets left
     while len(new_tweets) > 0 and oldest < new_tweets[0].id:
-        if debugging: print("getting all tweets before ID #%s" % (oldest_tweet))
+        log_it("getting all tweets before ID #%s" % oldest_tweet, 0)
         new_tweets = the_API.user_timeline(screen_name=screen_name, count=200, max_id=oldest_tweet)
         ret.extend(new_tweets)
         oldest_tweet = ret[-1].id - 1
-        if debugging: print("    ...%s tweets downloaded so far" % (len(ret)))
+        log_it("    ...%s tweets downloaded so far" % (len(ret)), 0)
     return [t for t in ret if (t.id > oldest)]
 
 def startup():
@@ -127,7 +136,7 @@ def startup():
            stopped running.
         2. that's it. Nothing else.
     """
-    if debugging: print('Starting up...')
+    log_it('Starting up...', 0)
     for id, username in Trump_twitter_accounts.items():
         try:
             with open("%s.%s" % (last_tweet_id_store, username)) as store:
@@ -136,7 +145,7 @@ def startup():
             with open("%s.%s" % (last_tweet_id_store, username), mode="w") as store:
                 store.write("-1")
             newest_id = -1
-        if debugging: print("about to get all tweets newer than ID #%s for account @%s" % (newest_id, username))
+        log_it("about to get all tweets newer than ID #%s for account @%s" % (newest_id, username))
         for tw in [t for t in get_new_tweets(screen_name=username, oldest=newest_id) if t.id > newest_id]:
             archive_tweet(tw.user.screen_name, tw.id_str, tw.text)
             time.sleep(1)
@@ -149,7 +158,7 @@ if __name__ == '__main__':
             l = TrumpListener()
             auth = OAuthHandler(consumer_key, consumer_secret)
             auth.set_access_token(access_token, access_token_secret)
-            if debugging: print("... OK, we're set up, and about to watch %s" % ', '.join(Trump_twitter_accounts))
+            log_it("... OK, we're set up, and about to watch %s" % ', '.join(Trump_twitter_accounts), 0)
             while True:
                 try:
                     stream = Stream(auth, l)
@@ -162,5 +171,6 @@ if __name__ == '__main__':
                     stream.disconnect()
                     break
     except pid.PidFileError:
-        if debugging: print("Already running! Quitting ...")
+        log_it("Already running! Quitting ...", 0)
         sys.exit()
+            
