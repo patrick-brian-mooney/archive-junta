a1a47222594f7715e8ba33c311001a3948c7974c
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2017-10-21 04:19:29 -0700

    20 Oct 2017: committing to older data; about to rebuild

--- find_deletions.py
+++ find_deletions.py
@@ -27,59 +27,11 @@ this program.
 """
 
 
-import csv, datetime, glob, os, requests
-import patrick_logger                               # https://github.com/patrick-brian-mooney/python-personal-library/
+import csv, requests
 
 
 home_dir = '/archive-junta'
 data_dir = '%s/data' % home_dir
-deleted_tweets_list = '%s/found_deleted_tweets.csv' % home_dir
+deleted_tweets_list = '%s/found_deleted_tweets.csv' % data_dir
 
 
-patrick_logger.verbosity_level = 5
-log = patrick_logger.Logger(name='find_deletions.py', logfile_paths = [ "%s/logs/find_deletions.py run on %s.log" % (home_dir, str(datetime.datetime.now())) ])
-
-
-log.log_it("We're starting...")
-
-
-def tweet_is_live(archival_url):
-    """Returns True if the tweet that was archived at ARCHIVAL_URL is still live on
-    Twitter; False if it is not.
-    """
-    return True     #FIXME
-
-
-if os.path.exists(deleted_tweets_list):
-    deleted_tweets_file = open(deleted_tweets_list, 'a', newline='')
-    csvwriter = csv.writer(deleted_tweets_file, dialect='unix')
-    log.log_it("INFO: successfully opened existing deleted tweets file for appending at %s." % deleted_tweets_list)
-else:
-    deleted_tweets_file = open(deleted_tweets_list, 'w', newline='')
-    csvwriter = csv.writer(deleted_tweets_file, dialect='unix')
-    csvwriter.writerow(['user ID', 'tweet ID', 'tweet text', 'archive URL', 'time deletion noticed'])
-    log.log_it("INFO: successfully created new deleted tweets file at %s." % deleted_tweets_list)
-
-for which_archive in sorted(glob.glob('%s/archive_*csv' % data_dir)):
-    log.log_it("\n\nINFO: about to begin searching for deletions in %s:" % which_archive)
-    with open(which_archive, 'r', newline='') as current_archive_file:
-        reader = csv.reader(current_archive_file, dialect='unix')
-        known_tweet_list = list(reader)
-        # Note that format here is a list of lists [['tweet', 'http://archived_tweet_URL'], ['(the same) tweet', 'https://archived_tweet_url'], ... ]
-        # In retrospect, I don't mind admitting I wish I'd tracked just a bit more data for each entry in the list.
-    for line_number, line in enumerate(known_tweet_list):
-        log.log_it(" INFO: we're looking at line # %d" % line_number, 2)
-        log.log_it('    ... contents of that line are: "%s" (length %d).' % (line, len(line)), 2)
-        tweet, archive_url = line
-        log.log_it("  INFO: we're checking for the tweet '%s' ..." % tweet, 3)
-        if not tweet_is_live(archive_url):
-            log.log_it("    --not found! Adding to deleted tweets list ...", 3)
-            username = which_archive.strip().lstrip(data_dir).strip().lstrip('archive_').strip().rstrip('.csv').strip()
-            tweet_id = archive_url.strip().lstrip('http://').lstrip('https://').strip().lstrip('web.archive.org/*/').lstrip('http://').lstrip('https://')
-            tweet_id = tweet_id.strip().lstrip('twitter.com/').lstrip(username).lstrip('/status/').strip()
-            csvwriter.writerow([ username, tweet_id, tweet, archive_url, str(datetime.datetime.now()) ])
-            log.log_it("         ...done.", 5)
-        else:
-            log.log_it("    --found!", 4)
-        
-deleted_tweets_file.close()
